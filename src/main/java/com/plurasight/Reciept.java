c50c18de7d0e1f1bb53a9d1f0841a2c038fba25a
josem71411-gif <josem71411@gmail.com>
2026-04-09 14:09:13 -0500

    Reciept for overpriced potatoes

--- src/main/java/com/plurasight/Reciept.java
+++ src/main/java/com/plurasight/Reciept.java
@@ -1,5 +1,14 @@
 package com.plurasight;
 
 public class Reciept {
+    public static void main(String[] args) {    String itemName = "Potatoes";
+    double itemPrice = 15.00;
+
+    int quanity = 3;
+
+    double total = itemPrice * quanity;
+
+    System.out.println("You bought " + quanity + " " + itemName + " for $" + total);
+    }
 }
 
