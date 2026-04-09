7f676fed3c7b3e9581b8466595a523c324c259e0
josem71411-gif <josem71411@gmail.com>
2026-04-09 11:57:27 -0500

    Feat: First two answers

---
+++ src/main/java/com/plurasight/VariableApp.java
@@ -0,0 +1,17 @@
+package com.plurasight;
+
+public class VariableApp {
+
+
+public static void main(String[] args) {
+        String favoriteColor = "yellow";
+        String message = "My favorite color is: ";
+        System.out.println(message + favoriteColor);
+
+        short yearStartedProgram = 2026;
+        System.out.println("I started YearUp United in " + yearStartedProgram);
+    }
+
+
+
+}
